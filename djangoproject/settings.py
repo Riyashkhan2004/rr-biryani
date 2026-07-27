c53d76bce27c034a52973df9650e530a2edefe7e
Riyash Khan <riyashkhan484@gmail.com>
2026-07-27 21:46:36 +0530

    Fix chappathi and parotta image filenames

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -1,7 +1,8 @@
 """
 Django settings for djangoproject project.
 """
-
+import dj_database_url
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,11 +74,13 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 
 # Database
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 # Password validation
